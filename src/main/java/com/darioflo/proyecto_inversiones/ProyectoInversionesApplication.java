6c0079f7ea827dfbb9c137867f4281cd89697e99
Darío Fernández <darioflo19@icloud.com>
2025-05-27 21:16:59 -0600

    Primer commit backend

---
+++ src/main/java/com/darioflo/proyecto_inversiones/ProyectoInversionesApplication.java
@@ -0,0 +1,13 @@
+package com.darioflo.proyecto_inversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInversionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInversionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darioflo/proyecto_inversiones/ProyectoInversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darioflo.proyecto_inversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
