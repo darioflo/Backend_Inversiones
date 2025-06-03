e988bc9fc62a20f122125ef1319896b44f39f1f4
Darío Fernández <darioflo19@icloud.com>
2025-06-03 16:55:46 -0600

    Backend terminado

--- src/main/java/com/darioflo/proyecto_inversiones/controllers/InversionesCuentaController.java
+++ src/main/java/com/darioflo/proyecto_inversiones/controllers/InversionesCuentaController.java
@@ -9,6 +9,9 @@ import com.darioflo.proyecto_inversiones.services.InversionesCuentaService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -32,6 +35,11 @@ public class InversionesCuentaController {
     @PostMapping()
     public InversionesCuentaModel crearInversionCuenta(@RequestBody InversionesCuentaModel nuevaInversion){
         return inversionesCuentaService.crearInversionCuenta(nuevaInversion);
+    }
+
+    @PutMapping("editar/{id}")
+    public InversionesCuentaModel editarInversionCuenta(@PathVariable String id, @RequestBody InversionesCuentaModel nuevaInversion) {
+        return inversionesCuentaService.editarInversionCuenta(id, nuevaInversion);
     }
     
 
--- src/main/java/com/darioflo/proyecto_inversiones/services/InversionesCuentaService.java
+++ src/main/java/com/darioflo/proyecto_inversiones/services/InversionesCuentaService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.darioflo.proyecto_inversiones.models.CuentaModel;
 import com.darioflo.proyecto_inversiones.models.InversionesCuentaModel;
 import com.darioflo.proyecto_inversiones.repositories.InversionesCuentaRepository;
 
@@ -13,6 +14,8 @@ public class InversionesCuentaService {
     
     @Autowired
     InversionesCuentaRepository inversionesCuentaRepository;
+    @Autowired
+    CuentaService cuentaService;
 
     public ArrayList<InversionesCuentaModel> obtenerInversionesCuentas(){
         return (ArrayList<InversionesCuentaModel>) inversionesCuentaRepository.findAll();
@@ -22,5 +25,31 @@ public class InversionesCuentaService {
         return inversionesCuentaRepository.save(nuevaInversion);
     }
 
+    public InversionesCuentaModel editarInversionCuenta(String id, InversionesCuentaModel datosActualizados){
+        return inversionesCuentaRepository.findById(id).map(inversionExistente -> {
+        inversionExistente.setCuenta(datosActualizados.getCuenta());
+        inversionExistente.setInversion(datosActualizados.getInversion());
+        inversionExistente.setEstaActiva(datosActualizados.isEstaActiva());
+        inversionExistente.setPlazo(datosActualizados.getPlazo());
+        inversionExistente.setTasa(datosActualizados.getTasa());
+        inversionExistente.setRendimientoAnual(datosActualizados.getRendimientoAnual());
+        inversionExistente.setSaldoInicial(datosActualizados.getSaldoInicial());
+        inversionExistente.setSaldoAlTermino(datosActualizados.getSaldoAlTermino());
+        inversionExistente.setInstruccionVencimiento(datosActualizados.getInstruccionVencimiento());
+        
+        String idCuenta = datosActualizados.getCuenta().getId();
+        Double nuevoSaldoInicial = datosActualizados.getSaldoInicial();
+        CuentaModel cuenta = cuentaService.obtenerCuentaPorID(idCuenta).orElse(null);
+        if (cuenta != null) {
+            Double saldoActual = cuenta.getSaldo().doubleValue();
+            Double saldoFinal = saldoActual - nuevoSaldoInicial;
+            if (saldoFinal >= 0) {
+                cuenta.setSaldo(saldoFinal.intValue());
+                cuentaService.actualizarSaldo(idCuenta, cuenta.getSaldo());
+            }
+        }
 
+        return inversionesCuentaRepository.save(inversionExistente);
+    }).orElse(null);
+    }
 }
