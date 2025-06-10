b7587d03d5ede5a7f8b582834445186107b6b6f0
Darío Fernández <darioflo19@icloud.com>
2025-06-09 20:34:30 -0600

    Modificado back

--- src/main/java/com/darioflo/proyecto_inversiones/models/InversionesModel.java
+++ src/main/java/com/darioflo/proyecto_inversiones/models/InversionesModel.java
@@ -3,7 +3,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "inversiones")
-
 public class InversionesModel {
     @Id
     private String idInversion;
