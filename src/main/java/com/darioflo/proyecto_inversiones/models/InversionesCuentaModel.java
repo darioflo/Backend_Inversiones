88c28d91f8fe61a2d51975a000e5b061036b2d65
Darío Fernández <darioflo19@icloud.com>
2025-06-11 14:40:15 -0600

    commit alt

--- src/main/java/com/darioflo/proyecto_inversiones/models/InversionesCuentaModel.java
+++ src/main/java/com/darioflo/proyecto_inversiones/models/InversionesCuentaModel.java
@@ -124,14 +124,26 @@ public class InversionesCuentaModel {
     private String numeroCuenta;
     private Integer saldo;
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public String getId() { 
+        return id; 
+    }
+    public void setId(String id) { 
+        this.id = id; 
+    }
     
-    public String getNumeroCuenta() { return numeroCuenta; }
-    public void setNumeroCuenta(String nuevoNumeroCuenta) { this.numeroCuenta = nuevoNumeroCuenta; }
+    public String getNumeroCuenta() { 
+        return numeroCuenta; 
+    }
+    public void setNumeroCuenta(String nuevoNumeroCuenta) { 
+        this.numeroCuenta = nuevoNumeroCuenta; 
+    }
     
-    public Integer getSaldo() { return saldo; }
-    public void setSaldo(Integer nuevoSaldo) { this.saldo = nuevoSaldo; }
+    public Integer getSaldo() { 
+        return saldo; 
+    }
+    public void setSaldo(Integer nuevoSaldo) { 
+        this.saldo = nuevoSaldo; 
+    }
 }
 
     public static class Inversion {
