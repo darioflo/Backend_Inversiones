93d4dd48cdab26e71a8f0e30dcbaa8359a31eeb1
Darío Fernández <darioflo19@icloud.com>
2025-06-26 22:23:15 -0600

    Antes de las pruebas.1

--- src/main/java/com/darioflo/proyecto_inversiones/controllers/UsuarioController.java
+++ src/main/java/com/darioflo/proyecto_inversiones/controllers/UsuarioController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin(origins = "*")
 public class UsuarioController {
     
-    
     @Autowired
     public UsuarioService usuarioService;
 
